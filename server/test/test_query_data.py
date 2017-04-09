22a3da72441a74f618d88fcc2dbc0c636df9e77b
Nikita Samsonov <nikita.sunwind@gmail.com>
2017-04-09 13:02:05 +0300

    Improved test suit of events querying

--- server/test/test_query_data.py
+++ server/test/test_query_data.py
@@ -3,10 +3,11 @@
 '''Query event data from the server
 '''
 
+from json import dumps
 from time import time
 from uuid import uuid4
 from pytest import mark
-from requests import get
+from requests import get, post
 from test.utils import EVENTS_URL, unzip_test_cases
 
 
@@ -17,21 +18,78 @@ class TestQueryData:
 
     action_cases = [
 
-        ('can-query-events-with-all-possible-parameters',
+        ('can-query-data-with-all-possible-filters',
          {
-             'format': 'numpy',
-             'id': uuid4(),
-             'series': 'demo',
-             'agent': 'Smith',
+             'format': 'json',
+             'id': str(uuid4()),
+             'series': 'series-{}'.format(str(uuid4())),
+             'agent': 'agent-{}'.format(str(uuid4())),
              'since': time() - 1000,
-             'till': time(),
-             'columns': ['_timestamp', 'roundtrip_delay'],
+             'till': time() + 1000,
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
          },
          200,
          None),
 
-        ('can-query-events-without-parameters',
-         {},
+        ('can-query-data-without-filters',
+         {
+         },
+         200,
+         None),
+
+        ('can-query-data-by-id',
+         {
+             'id': str(uuid4()),
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-data-by-series',
+         {
+             'series': 'series-{}'.format(str(uuid4())),
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-data-by-agent',
+         {
+             'agent': 'agent-{}'.format(str(uuid4())),
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-data-by-time-period',
+         {
+             'since': time() - 1000,
+             'till': time() + 1000,
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-data-by-time-since',
+         {
+             'since': time() - 1000,
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-data-by-time-till',
+         {
+             'till': time() + 1000,
+             'columns': ['_id', '_timestamp', 'roundtrip_delay'],
+         },
+         200,
+         None),
+
+        ('can-query-nonexistent-column',
+         {
+             'columns': ['_id', 'weird_column'],
+         },
          200,
          None),
 
@@ -40,14 +98,51 @@ class TestQueryData:
     ids, argvalues = unzip_test_cases(action_cases)
 
     @mark.parametrize('params,code,message', argvalues, ids=ids)
-    def test_query_events(self, params, code, message):
+    def test_can_query_data(self, params, code, message):
+
+        event_id = params.get('id', str(uuid4()))
+
+        payload = {
+            '_id': event_id,
+            '_series': params.get('series', 'demo'),
+            '_agent': params.get('agent', 'Smith'),
+            '_timestamp': time(),
+            'roundtrip_delay': 0.001,
+        }
+
+        response = post(EVENTS_URL, data=dumps(payload))
+        assert response.status_code == 200
 
         response = get(EVENTS_URL, params=params)
-
         assert response.status_code == code
 
         if message:
             assert response.text == message
+            return
+
+        results = response.json()
+        assert isinstance(results, list)
+
+        # Our newly loaded event should be present in results
+        result_ids = set(event['_id'] for event in results)
+        assert event_id in result_ids
+
+        # Server should return either all selected columns
+        # or all existing columns if no one is selected
+        selected_columns = set(params.get('columns', []))
+        existing_columns = set(payload.keys())
+        if selected_columns:
+            columns = selected_columns
         else:
-            results = response.json()
-            assert isinstance(results, list)
+            columns = existing_columns
+
+        for event in results:
+            for column in columns:
+                assert column in event
+                # Non-existing columns must be included with value None
+                if column not in existing_columns:
+                    assert event[column] is None
+
+            # Existing but not selected columns must not be included
+            for column in existing_columns - columns:
+                assert column not in event
